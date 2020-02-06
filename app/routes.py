1c0d66e7dc4b734f8b8ecacbaf41aacd50e0e83d
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-02-06 17:07:06 +0000

    search route works

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for
 # from scraper import scrape
 from . import scraper
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     data = scraper.scrape("https://www.bbc.co.uk/search?q=boris&sa_f=search-product&filter=news&suggid=")
     return render_template('index.html', data=data)
+
+@app.route('/search', methods=['POST'])
+def search():
+    return 'search worked!'
