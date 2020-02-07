e45e797ac68b8e5dd4d85a140b9b29fdf4b41126
bcht201 <brianting1227@gmail.com>
2020-02-07 14:53:13 +0000

    write scrape data to database and read to html

--- app/routes.py
+++ app/routes.py
@@ -49,47 +49,13 @@ def index():
 @app.route('/search', methods=['POST'])
 def search():
     keyword = request.form['search']
-    scrape('https://www.bbc.co.uk/search?q=' + keyword + '&filter=news', keyword)
-    return render_template('index.html', )
-
-
-
-
-
-    # scrape()
-    # search_query = request.form['search']
-    # sql = text("SELECT keyword FROM scrapeData WHERE keyword LIKE '%" + search_query + "%'")
-    # execute = db.engine.execute(sql)
-    # result = [row for rows in execute]
-    # print(result)
-    return "Hello"
-    # already_scraped = db.data_scrape.filter(db.data_scrape.keyword == search_query)
-    # print(already_scraped)
-    # if len(already_scraped) > 1:
-    #     return "Already scraped"
-    # else:
-    #     return "Not scraped yet"
-
-    # search_query = request.form['search']
-    # search_query = search_query.replace(' ', '+')
-    # if search_query is None:
-    #     return redirect('/')
-    # else:
-    #     time.sleep(random.randint(0, 3))
-    #     data = scraper.scrape('https://www.bbc.co.uk/search?q=' + search_query + '&filter=news')
-
-
-    #     return render_template('index.html', infos=data)
-	
-
-@app.route('/populate', methods=["POST"])
-def add_data():
-    content = scrapeData(
-        title=request.form["title"],
-        link=request.form["link"],
-        source=request.form["source"],
-        keyword=request.form["keyword"]
-    )
-    db.session.add(content)
-    db.session.commit()
-    return "you populated the db"
+    if keyword is None:
+        return redirect('/')
+    else:
+        time.sleep(random.randint(0, 3))
+        scrape('https://www.bbc.co.uk/search?q=' + keyword + '&filter=news', keyword)
+        sql = text("SELECT title, link FROM scraped_data_all WHERE keyword='" + keyword + "'")
+        execute = db.engine.execute(sql)
+        infos = [row for row in execute]
+        print(infos)
+        return render_template('index.html', infos = infos)
