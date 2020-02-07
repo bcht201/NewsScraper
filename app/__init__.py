eacfa1ae78484dd6c75aa5409f2ff98a78108d8a
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-02-07 10:27:22 +0000

    add database setup and table structure

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 from . import routes
 from . import scraper
+from app.db_classes.db_class import Data_Scrape
 from app import app
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,16 @@ from flask import Flask, render_template, request, redirect, url_for
 from . import scraper
 import time
 import random
+# from app.db_classes.db_class import Data_Scrape
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scrapes.db'
+
+db = SQLAlchemy(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
 
 @app.route('/', methods=['GET'])
 def index():
