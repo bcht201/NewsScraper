60829dd11a912182cad48c5bfe49d5367e30f09b
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-02-12 14:30:57 +0000

    simplify database functions and clear db

--- application/database.py
+++ application/database.py
@@ -4,7 +4,8 @@ from application.models import Keyword, User_Search, User
 
 
 def read_db(sql):
-    return db.engine.execute(sql)
+    execute = db.engine.execute(sql)
+    return [row for row in execute]
 
 def write_db(content):
     db.session.add(content)
@@ -12,8 +13,7 @@ def write_db(content):
 
 def db_check_keyword(keyword):
     sql = text("SELECT id, keyword FROM keyword WHERE keyword='" + keyword + "'")
-    execute = read_db(sql)
-    return [row for row in execute]
+    return read_db(sql)
 
 def write_keyword(keyword):
     obj = Keyword(
@@ -23,8 +23,7 @@ def write_keyword(keyword):
 
 def get_next_kw_id():
     sql = text("SELECT id FROM keyword ORDER BY id DESC")
-    execute = read_db(sql)
-    return [row for row in execute]
+    return read_db(sql)
 
 def write_user_keyword(keyword, user_id):
     keyword_and_id = db_check_keyword(keyword)
@@ -36,18 +35,15 @@ def write_user_keyword(keyword, user_id):
     
 def get_keyword_id(keyword):
     sql = text("SELECT id FROM keyword WHERE keyword='" + keyword + "'")
-    execute = read_db(sql)
-    return [row for row in execute]
+    return read_db(sql)
 
 def recent_keywords(user_id):
     sql = text("SELECT DISTINCT user_search.id, keyword.keyword FROM user_search JOIN keyword ON user_search.keyword_id = keyword.id WHERE user_search.user_id = " + user_id)
-    execute = read_db(sql)
-    return [row for row in execute]
+    return read_db(sql)
 
 def get_what_you_just_searched(key_id):
     sql = text("SELECT title, link, keyword, source FROM scraped_data_all WHERE keyword=" + str(key_id))
-    execute = read_db(sql)
-    return [row for row in execute]
+    return read_db(sql)
 
 def db_update_settings(BBC, DM, TS, user_id):
     x = db.session.query(User).get(user_id)
@@ -56,10 +52,13 @@ def db_update_settings(BBC, DM, TS, user_id):
     x.DM_quant = DM
     db.session.commit()
 
+def check_user(email):
+    sql = text("SELECT * FROM user WHERE email='" + email + "'")
+    return read_db(sql)
+
 def get_current_settings(user_id):
     sql = text('SELECT BBC_quant, TS_quant, DM_quant FROM user WHERE id = ' + str(user_id))
-    execute = read_db(sql)
-    return [row for row in execute]
+    return read_db(sql)
 
 def delete():
     sql = text("DELETE FROM scraped_data_all")
